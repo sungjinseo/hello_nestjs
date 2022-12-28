179f9c7cf676c7dcbfb0bc06104a1468dcfb0967
greatSeo <sjseo85@gmail.com>
2022-12-29 02:30:10 +0900

    merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
@@ -11,15 +10,5 @@ import { AuthModule } from './auth/auth.module';
     BoardsModule,
     AuthModule
   ],
-=======
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-
-@Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
->>>>>>> 00ad6bb9ecf9799589fd871ee7dc0e8f1c2af01f
 })
 export class AppModule {}
